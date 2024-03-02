d657e506140b265313a444e68a9fca9d2c4b1fcb
Andrea Cubillos <Andreav.cubillosp@konradlorenz.edu.co>
2024-03-02 11:02:26 -0500

    Se ajustó el video
    Se ajusto un corto  video de algunas de las películas  de este personaje a la sección "Sobre él".

--- js/scripts.js
+++ js/scripts.js
@@ -53,3 +53,69 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+
+let score = 0;
+const gameContainer = document.getElementById("game-container");
+const scoreDisplay = document.getElementById("score");
+
+function randomPosition() {
+    const x = Math.random() * (gameContainer.offsetWidth - 50);
+    const y = Math.random() * (gameContainer.offsetHeight - 50);
+    return { x, y };
+}
+
+function createSandler() {
+    const sandler = document.createElement("img");
+    sandler.src = "https://lens-storage.storage.googleapis.com/png/d185c35252414d8f9fe4c7c44398d1da";
+    sandler.classList.add("adam-sandler");
+    const position = randomPosition();
+    sandler.style.position = "absolute"; 
+    sandler.style.left = position.x + "px";
+    sandler.style.top = position.y + "px";
+    sandler.style.width = "50px";
+    sandler.style.height = "50px";
+    sandler.addEventListener("click", () => {
+        score++;
+        scoreDisplay.textContent = "Puntaje: " + score;
+        sandler.remove();
+    });
+    gameContainer.appendChild(sandler);
+}
+
+const intervalId = setInterval(createSandler, 1000);
+
+setTimeout(() => {
+    clearInterval(intervalId);
+    alert("¡Juego terminado! Tu puntaje: " + score);
+    document.getElementById('myModal').style.display = "block";
+}, 20000);
+
+window.onclick = function(event) {
+    var modal = document.getElementById('myModal');
+    if (event.target == modal) {
+        modal.style.display = "none";
+        restartGame();
+    }
+}
+
+document.getElementById('openModalButton').onclick = function() {
+    document.getElementById('myModal').style.display = "block";
+    restartGame();
+}
+
+function restartGame() {
+    score = 0;
+    scoreDisplay.textContent = "Puntaje: " + score;
+    const sandlerElements = document.querySelectorAll('.adam-sandler');
+    sandlerElements.forEach(element => {
+        element.remove();
+    });
+    const restartIntervalId = setInterval(createSandler, 1000);
+    setTimeout(() => {
+        clearInterval(restartIntervalId);
+        alert("¡Juego terminado! Tu puntaje es: " + score);
+        document.getElementById('myModal').style.display = "block";
+    }, 20000);
+}
+
